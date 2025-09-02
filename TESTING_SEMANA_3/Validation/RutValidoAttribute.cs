3a2b0282aa0d531c43554905aaf9556d2b99bdc1
Hpeters <hans.peters@correoaiep.cl>
2025-09-02 13:18:04 -0400

    actualizacion de verificacion de validacion de rut con modulo 11

--- TESTING_SEMANA_3/Validation/RutValidoAttribute.cs
+++ TESTING_SEMANA_3/Validation/RutValidoAttribute.cs
@@ -1,27 +1,66 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
 
 namespace ProyectoFichaMedica.Validation
 {
     public class RutValidoAttribute : ValidationAttribute
     {
-        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
             if (value is not string rut || string.IsNullOrEmpty(rut))
             {
-                return ValidationResult.Success!;
+                return ValidationResult.Success; 
             }
 
-            rut = rut.Replace(".", "");
-
-            var rutPattern = @"^(\d{1,8}-[\dkK])$";
-
-            if (!Regex.IsMatch(rut, rutPattern))
+            try
             {
-                return new ValidationResult(ErrorMessage ?? "El formato del RUT no es válido.");
-            }
+                rut = rut.ToUpper().Replace(".", "").Replace("-", "");
 
-            return ValidationResult.Success!;
+                string cuerpo = rut.Substring(0, rut.Length - 1);
+                char dv = rut[rut.Length - 1];
+
+                //modulo 11
+                int suma = 0;
+                int multiplo = 2;
+
+                for (int i = cuerpo.Length - 1; i >= 0; i--)
+                {
+                    suma += int.Parse(cuerpo[i].ToString()) * multiplo;
+                    if (multiplo < 7)
+                    {
+                        multiplo++;
+                    }
+                    else
+                    {
+                        multiplo = 2;
+                    }
+                }
+
+                int digitoEsperado = 11 - (suma % 11);
+                string dvEsperado = digitoEsperado.ToString();
+
+                if (digitoEsperado == 11)
+                {
+                    dvEsperado = "0";
+                }
+                if (digitoEsperado == 10)
+                {
+                    dvEsperado = "K";
+                }
+
+                if (dv.ToString() == dvEsperado)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult(ErrorMessage ?? "El RUT no es válido (dígito verificador incorrecto).");
+                }
+            }
+            catch (Exception)
+            {
+                return new ValidationResult(ErrorMessage ?? "El RUT no tiene un formato válido.");
+            }
         }
     }
+    
 }
